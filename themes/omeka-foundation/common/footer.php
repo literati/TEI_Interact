31ce847f1a943d9e9bfa1d967835fda39e71df14
jason peak <jpeak5@lsu.edu>
2012-09-30 10:22:41 -0500

    themes are broken out into their own repos

--- themes/omeka-foundation/common/footer.php
+++
@@ -1,9 +0,0 @@
-
-<div class="row">
-	<div id="footer" class="twelve columns">
-		<?php admin_plugin_footer(); ?>
-		<ul class="pagination">
-			<?php echo custom_public_nav_header(); ?>
-		</ul>
-	</div>
-</div>
