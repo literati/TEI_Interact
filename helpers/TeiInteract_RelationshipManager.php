010329bc8c76743a513ab1fcfeefd16c079fc1ed
jason peak <jpeak5@lsu.edu>
2012-10-02 21:39:01 -0500

    this is mainly a stub from the end of summer development effort
    migrating it here, for what it's worth

---
+++ helpers/TeiInteract_RelationshipManager.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of TeiInteract_RelationshipManager
+ *
+ * @author jpeak5
+ */
+class TeiInteract_RelationshipManager {
+    //put your code here
+}
+
+?>
