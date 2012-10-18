fa31e077129fa7635848459c84edac2371210111
jason peak <jpeak5@lsu.edu>
2012-10-17 20:51:49 -0500

    formatting for readability

--- helpers/TeiInteract_TripleSet.php
+++ helpers/TeiInteract_TripleSet.php
@@ -30,67 +30,73 @@ class TeiInteract_TripleSet {
          * 
          */
 
-        $tripleSet = new TeiInteract_TripleSet();
-        $tripleSet->rootTitle = $title;
-        
-        $tripleSet->rootItems = teiInteract_getItemsByDCTitle($title); //step 1
+        $tripleSet              = new TeiInteract_TripleSet();
+        $tripleSet->rootTitle   = $title;
+        $tripleSet->rootItems   = teiInteract_getItemsByDCTitle($title); //step 1
         
         if(count($tripleSet->rootItems)>0){
-            debug(sprintf("%s - found %d matches for search term '%s'; first found match ID = %d", get_class(), count($tripleSet->rootItems), $title, $tripleSet->rootItems[0]->id));
-        $tripleSet->triples = array();
-        debug(sprintf("%s - processing %d item records into one triple Set", get_class(), count($tripleSet->rootItems)));
-        
-        foreach($tripleSet->rootItems as $item) {  
-            debug(sprintf("beginning to build triples sourced from item with id = %d",$item->id));
-            $tripleSet->triples[$item->id] = array();
+            debug(sprintf(
+                "%s - found %d matches for search term '%s'; first found match ID = %d", 
+                get_class(), count($tripleSet->rootItems), $title, $tripleSet->rootItems[0]->id));
+
+            $tripleSet->triples = array();
+            debug(sprintf(
+                "%s - processing %d item records into one triple Set", 
+                get_class(), count($tripleSet->rootItems)));
             
-            $irirTbl = new ItemRelationsItemRelationTable('ItemRelationsItemRelation', get_db());
+            foreach($tripleSet->rootItems as $item) {  
+                debug(sprintf(
+                    "beginning to build triples sourced from item with id = %d",
+                    $item->id));
 
-            $subjects = array(); //where the searchStr item is subject
-            $objects = array(); //where the searchStr item is object
+                $tripleSet->triples[$item->id] = array();
+                
+                $irirTbl    = new ItemRelationsItemRelationTable(
+                    'ItemRelationsItemRelation', get_db());
 
-
-            $subjects = array_merge($subjects, $irirTbl->findBySubjectItemId($item->id));
-            $objects = array_merge($objects, $irirTbl->findByObjectItemId($item->id));
-
-            debug(sprintf("found %d matches in the relations table 
-                (%d subjects, %d objects) for item with id = %d"
-                            , count($subjects) + count($objects)
-                            , count($subjects)
-                            , count($objects)
-                            , $item->id
-                    )
-            );
-
-            $relations = array_merge($subjects,$objects);
+                $subjects   = array(); //where the searchStr item is subject
+                $objects    = array(); //where the searchStr item is object
 
 
-            $itemTbl = get_db()->getTable('Item');
-            $irpTbl = get_db()->getTable('ItemRelationsProperty');
+                $subjects   = array_merge($subjects, $irirTbl->findBySubjectItemId($item->id));
+                $objects    = array_merge($objects, $irirTbl->findByObjectItemId($item->id));
 
-            foreach ($relations as $relation) {//ItemRelations
-                $triple = new TeiInteract_Triple;
-                $triple->key = $item->id;
-                $triple->keyTitle = $title;
-                $triple->irIR = $relation;
-                $triple->object = $itemTbl->find($relation->object_item_id);
-                $triple->subject = $itemTbl->find($relation->subject_item_id);
-                $triple->irProp = $irpTbl->find($relation->property_id);
-
-
-                $tripleSet->triples[$item->id][] = $triple;
-                $triple->dumpTriple();
-                }
-            }
-            debug(sprintf("returning triple set with rootTitle '%s', %d rootItems, and %d triples",
-                        $tripleSet->rootTitle,
-                        count($tripleSet->rootItems),
-                        count($tripleSet->triples)
+                debug(sprintf("found %d matches in the relations table 
+                    (%d subjects, %d objects) for item with id = %d"
+                                , count($subjects) + count($objects)
+                                , count($subjects)
+                                , count($objects)
+                                , $item->id
                         )
-                    );
-        }
+                );
+
+                $relations  = array_merge($subjects,$objects);
+                $itemTbl    = get_db()->getTable('Item');
+                $irpTbl     = get_db()->getTable('ItemRelationsProperty');
+
+                foreach ($relations as $relation) {//ItemRelations
+                    $triple             = new TeiInteract_Triple;
+                    $triple->key        = $item->id;
+                    $triple->keyTitle   = $title;
+                    $triple->irIR       = $relation;
+                    $triple->object     = $itemTbl->find($relation->object_item_id);
+                    $triple->subject    = $itemTbl->find($relation->subject_item_id);
+                    $triple->irProp     = $irpTbl->find($relation->property_id);
+
+
+                    $tripleSet->triples[$item->id][] = $triple;
+                    $triple->dumpTriple();
+                    }
+                }
+                debug(sprintf("returning triple set with rootTitle '%s', %d rootItems, and %d triples",
+                            $tripleSet->rootTitle,
+                            count($tripleSet->rootItems),
+                            count($tripleSet->triples)
+                            )
+                        );
+            }
             return $tripleSet;
-        } 
+    } 
     
     
     
