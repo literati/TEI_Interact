9d6e6c86221bca30a80c71d0377ca6c135b38aac
jason peak <jpeak5@lsu.edu>
2012-11-29 21:25:35 -0600

    visual cleanups

--- controllers/ServicesController.php
+++ controllers/ServicesController.php
@@ -8,16 +8,18 @@
 class TeiInteract_ServicesController extends Omeka_Controller_Action {
 
     public function lookupAction(){
-        $searchStr = teiInteract_normalizeText($this->getRequest()->getParam('hover'));
+        $searchStr              = teiInteract_normalizeText($this->getRequest()->getParam('hover'));
+
         debug(sprintf("user mouse is hovering over TEI tag with value %s", $searchStr));
-        $el = $this->getRequest()->getParam('element');
-        $this->view->search = $searchStr;
-        $this->view->element = $el;
-        $this->view->type = $this->getRequest()->getParam('type');
+
+        $el                     = $this->getRequest()->getParam('element');
+        $this->view->search     = $searchStr;
+        $this->view->element    = $el;
+        $this->view->type       = $this->getRequest()->getParam('type');
         
         debug('calling new static help for triple');
-        $this->view->tripleSet = TeiInteract_TripleSet::getTriplesSetForTitle($searchStr);
-        
+
+        $this->view->tripleSet  = TeiInteract_TripleSet::getTriplesSetForTitle($searchStr);
     }
 }
 
